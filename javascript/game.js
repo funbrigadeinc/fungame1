aea9ac0df7c768ca76b7768e4a5b2ff16fb40946
funbrigadeinc <70872806+funbrigadeinc@users.noreply.github.com>
2020-09-07 16:23:00 +0530

    Add files via upload

--- javascript/game.js
+++ javascript/game.js
@@ -1,14 +1,25 @@
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(100, 100, "images/jerry.JPG", 10, 120, "image");
+
+    piece11 = new component(100, 100, "images/row-1-col-1.PNG", 10, 10, "image");
+    piece12 = new component(100, 100, "images/row-1-col-2.PNG", 110, 10, "image");
+    piece13 = new component(100, 100, "images/row-1-col-3.PNG", 210, 10, "image");
+
+    piece21 = new component(100, 100, "images/row-2-col-1.PNG", 10, 110, "image");
+    piece22 = new component(100, 100, "images/row-2-col-2.PNG", 110, 110, "image");
+    piece23 = new component(100, 100, "images/row-2-col-3.PNG", 210, 110, "image");
+
+    piece31 = new component(100, 100, "images/row-3-col-1.PNG", 10, 210, "image");
+    piece32 = new component(100, 100, "images/row-3-col-2.PNG", 110, 210, "image");
+    piece33 = new component(100, 100, "images/row-3-col-3.PNG", 210, 210, "image");
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 500;
+        this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[20]);
         this.interval = setInterval(updateGameArea, 20);
@@ -47,27 +58,87 @@ function component(width, height, color, x, y, type) {
 
 function updateGameArea() {
   myGameArea.clear();
-  myGamePiece.newPos();
-  myGamePiece.update();
+  piece11.newPos();
+  piece11.update();
+  piece12.newPos();
+  piece12.update();
+  piece13.newPos();
+  piece13.update();
+
+  piece21.newPos();
+  piece21.update();
+  piece22.newPos();
+  piece22.update();
+  piece23.newPos();
+  piece23.update();
+
+  piece31.newPos();
+  piece31.update();
+  piece32.newPos();
+  piece32.update();
+  piece33.newPos();
+  piece33.update();
 }
 
-function moveup(){
-  myGamePiece.speedY -=1;
-}
+function move1up()   { piece11.speedY -=2; }
+function move1down() { piece11.speedY +=2; }
+function move1left() { piece11.speedX -=2; }
+function move1right(){ piece11.speedX +=2; }
+function stop1Move() { piece11.speedX = 0;
+                       piece11.speedY = 0; }
 
-function movedown(){
-  myGamePiece.speedY +=1;
-}
+function move2up()   { piece12.speedY -=2; }
+function move2down() { piece12.speedY +=2; }
+function move2left() { piece12.speedX -=2; }
+function move2right(){ piece12.speedX +=2; }
+function stop2Move() { piece12.speedX = 0;
+                       piece12.speedY = 0; }
 
-function moveleft(){
-  myGamePiece.speedX -=1;
-}
+function move3up()   { piece13.speedY -=2; }
+function move3down() { piece13.speedY +=2; }
+function move3left() { piece13.speedX -=2; }
+function move3right(){ piece13.speedX +=2; }
+function stop3Move() { piece13.speedX = 0;
+                       piece13.speedY = 0; }
 
-function moveright(){
-  myGamePiece.speedX +=1;
-}
+function move4up()   { piece21.speedY -=2; }
+function move4down() { piece21.speedY +=2; }
+function move4left() { piece21.speedX -=2; }
+function move4right(){ piece21.speedX +=2; }
+function stop4Move() { piece21.speedX = 0;
+                       piece21.speedY = 0; }
 
-function stopMove() {
-  myGamePiece.speedX = 0;
-  myGamePiece.speedY = 0;
-}
+function move5up()   { piece22.speedY -=2; }
+function move5down() { piece22.speedY +=2; }
+function move5left() { piece22.speedX -=2; }
+function move5right(){ piece22.speedX +=2; }
+function stop5Move() { piece22.speedX = 0;
+                      piece22.speedY = 0; }
+
+function move6up()   { piece23.speedY -=2; }
+function move6down() { piece23.speedY +=2; }
+function move6left() { piece23.speedX -=2; }
+function move6right(){ piece23.speedX +=2; }
+function stop6Move() { piece23.speedX = 0;
+                       piece23.speedY = 0; }
+
+function move7up()   { piece31.speedY -=2; }
+function move7down() { piece31.speedY +=2; }
+function move7left() { piece31.speedX -=2; }
+function move7right(){ piece31.speedX +=2; }
+function stop7Move() { piece31.speedX = 0;
+                       piece31.speedY = 0; }
+
+function move8up()   { piece32.speedY -=2; }
+function move8down() { piece32.speedY +=2; }
+function move8left() { piece32.speedX -=2; }
+function move8right(){ piece32.speedX +=2; }
+function stop8Move() { piece32.speedX = 0;
+                       piece32.speedY = 0; }
+
+function move9up()   { piece33.speedY -=2; }
+function move9down() { piece33.speedY +=2; }
+function move9left() { piece33.speedX -=2; }
+function move9right(){ piece33.speedX +=2; }
+function stop9Move() { piece33.speedX = 0;
+                       piece33.speedY = 0; }
